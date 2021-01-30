87d7b9748b2dbba325b6883781d81e44c572046d
Samuelves <ivessamuel64@gmail.com>
2021-01-30 16:36:42 -0300

    :tada: Controle de Rotas Prontos

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+  MIT License
+
+  Copyright (c) 2021 Ives Samuel
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ */
+
+namespace app\Controllers;
+
+/**
+ * Description of HomeController
+ *
+ * @author Ives Samuel
+ */
+class HomeController
+{
+    public function index($request)
+    {
+        require_once __DIR__."/../Views/home/index.phtml";
+    }
+}
