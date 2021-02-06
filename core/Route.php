9074c9401d2e2c6d5c2c6a1e07d81668f055875a
Ives Samuel <48871044+Samuelves@users.noreply.github.com>
2021-02-06 13:49:13 -0800

    Update Route.php

--- core/Route.php
+++ core/Route.php
@@ -95,10 +95,10 @@ class Route
                 $found = true;
                 $controller = $route[1];
                 $action = $route[2];
-                /*$auth = new Auth;
+                $auth = new Auth;
                 if(isset($route[3]) && $route[3] == 'auth' && !$auth->check()){
                     $action = 'forbiden';
-                }*/
+                }
                 break;
             }
         }
